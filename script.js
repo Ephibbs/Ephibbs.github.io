7c82da782f04267f90a0dac655bca6b8af3fcde8
Evan Phibbs <evan.phibbs@yahoo.com>
2014-09-18 20:49:06 -0500

    updated stuff

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 // JavaScript Document
 var wasabove;
 var threshold = $(window).height()*.7;
+var MAXSTARS = 100
 var numstars = 0;
-$(document).ready(function () {
 
+$(document).ready(function () {
+	randomshootingstars();
 	if ($(window).scrollTop() > threshold) {
 		$("header").fadeIn();
 		wasabove = false;
@@ -13,21 +15,15 @@ $(document).ready(function () {
 		wasabove = true;
 	}
 	
-	$("#hero").css("height", $(window).height()+"px");
-	$("section").css("min-height", $(window).height()+"px");
+	$("section, #starholder").css("min-height", $(window).height()+"px");
+	$("header").css("top", $("#resume").offset().top+"px")
 	$("#starholder").css("top", $(window).height());
 	$("#starholder").width($("#resume").width())
-	$("#starholder").height($("#resume").height())
-	if ($(window).scrollTop() + $(window).height() > $("#hero").height() && $(window).scrollTop() < $("#hero").height() + $("#resume").height()) {
-		randomshootingstars();
-		$("#resume .content").fadeIn();
-	}
-  
- /*var $window = $(window);
+  	var $window = $(window);
     $('*[data-type="parallax"]').each(function(){
         var $bgobj = $(this); // assigning the object
      
-        $(window).scroll(function() {
+        $window.scroll(function() {
             var yPos = -($window.scrollTop() / $bgobj.data('speed')); 
              
             // Put together our final background position
@@ -36,11 +32,60 @@ $(document).ready(function () {
             // Move the background
             $bgobj.css({ "margin-top": coords});
         }); 
-    }); */           
+    });  
+$("section").each(function() {
+	var This = $(this);
+	$(window).scroll(function() { 
+		if ($(window).scrollTop() + $(window).height() > This.offset().top + This.height()/2 && $(window).scrollTop() < This.offset().top + This.height()) {
+			This.find(".content").fadeTo("slow", 1);
+			
+			var color = This.css("background-color");
+			var r = color.match(RegExp("[0-9]+"));
+			var g = color.replace(r, "").match(RegExp("[0-9]+"));
+			var b = color.replace(r, "").replace(g, "").match(RegExp("[0-9]+"));
+			var color1 = "rgba("+r+","+g+","+b+",1)";
+			/*
+			r = Math.floor(parseInt(r ,16)/2).toString(16);
+			g = Math.floor(parseInt(g, 16)/2).toString(16);
+			b = Math.floor(parseInt(b, 16)/2).toString(16);
+			var color2 = "rgba("+r+","+g+","+b+",1)";
+			*/
+			$("header #logo a").css("color", color1);
+			/*
+			$("header #logo a:hover").css("color", color2);
+			*/
+		}
+	});
+});       
 });
-
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
 $(window).scroll(function() {
-	randomshootingstars();
+	var top = 3*$(window).scrollTop();
+	var height = $("#hero").height();
+	if (top > height) {top = height;}
+	if (top < 0) {top = 0;}
+	console.log($("header").offset().top);
+	if ($("header").offset().top <= $(window).scrollTop()) {
+		$("header").css("position", "fixed").css("top", "0");
+	}
+	if ($("#resume").offset().top > $(window).scrollTop()) {
+		$("header").css("position", "absolute").css("top", $("#resume").offset().top+"px");
+	}
+	$("#hero h1, #hero nav").css("opacity", 1-(top/height));
+	
 	if ($(window).scrollTop() > threshold && wasabove) {
 		$("header").fadeIn();
 		wasabove = false
@@ -49,13 +94,7 @@ $(window).scroll(function() {
 		$("header").fadeOut();
 		wasabove = true
 	}
-	if ($(window).scrollTop() + $(window).height() > $("#hero").height() + 200 && $(window).scrollTop() < $("#hero").height() + $("#resume").height()) {
-		randomshootingstars();
-		$("#resume .content").fadeIn();
-	}
-	if ($(window).scrollTop() + $(window).height() > $("#hero").height() + $("#resume").height()+ 600 && $(window).scrollTop() < $("#hero").height() + $("#resume").height()+$("#projects").height()) {
-		$("#projects .content").fadeIn();
-	}
+	
 	if ($(window).scrollTop() + $(window).height() > $("#projects svg").offset().top && $(window).scrollTop() < $("#projects svg").offset().top) {
 		$("#projects svg").css("fill", "#FFF034");
 	} else {
@@ -72,7 +111,7 @@ $(window).resize(function () {
 });
 function randomshootingstars() {
 	$("#starholder").css("height", $("#resume").height());
-	while(numstars < 100) {
+	while(numstars < 100) {//var i = 0; i <= numstars; i++) {
 			var diam = Math.random()*20;
 			var speed = Math.random()*10+3;
 			var styles = {
